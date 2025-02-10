d3f0be55f71054c76d024686adbb7d1774e587c2
canhlong1430 <73382249+canhlong1430@users.noreply.github.com>
2025-02-10 21:43:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def update_google_sheet(data):
         worksheet.append_row(["Title", "Summary", "Link"])
 
     # Ghi đè thời gian cập nhật vào ô A1
-    worksheet.update(range_name='A1', values=[[f"Cập nhật lúc: {current_time} (GMT+7)"]])
+    worksheet.update(range_name='D1', values=[[f"Cập nhật lúc: {current_time} (GMT+7)"]])
 
     # Kiểm tra các link đã tồn tại
     existing_links = set()
